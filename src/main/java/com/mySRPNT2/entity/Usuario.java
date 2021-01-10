0db20af44dc6a984cbf105022fc5ce091bf614ce
JuanGiacalone <juanpablogiacalone8@gmail.com>
2021-01-10 02:34:06 -0300

    agregue class _UsuarioBicicleta

--- src/main/java/com/mySRPNT2/entity/Usuario.java
+++ src/main/java/com/mySRPNT2/entity/Usuario.java
@@ -3,12 +3,76 @@
  */
 package com.mySRPNT2.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 /**
  * @author Juan
  *
  */
+@Entity(name = "Usuario")
+@Table(
+		name = "usuario"
+		/*uniqueConstraints = {
+				@UniqueConstraint(name = "", columnNames = "")
+		}*/ /// usar cuando una constraint toma nombre dificil de leer.
+		)
 public class Usuario {
-
+	
+	@Id
+	@SequenceGenerator(
+			name = "usuario_sequence",
+			sequenceName  = "usuario_sequence",
+			allocationSize = 1 )
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "usuario_sequence"
+			)
+	@Column(
+			name = "id",
+			updatable = false
+			)
+	private long id;
+	
+	
+	/*TODO*/
+	private String contrasenia;
+	
+	
+	@Column( 
+			name = "primerNombre",
+			nullable = false,
+			columnDefinition = "TEXT"
+			)
+	private String primerNombre;
+	
+	@Column( 
+			name = "apellido",
+			nullable = false,
+			columnDefinition = "TEXT"
+			)
+	private String apellido;
+	
+	/*TODO*/
+	@Column( 
+			name = "fechaNac",
+			nullable = false,
+			columnDefinition = "TEXT"
+			)
+	private String fechaNac;
+	
+	
+	
+	
+	
+	
+	
+	
 	/**
 	 * 
 	 */
---
+++ src/main/java/com/mySRPNT2/entity/_UsuarioBicicleta.java
@@ -0,0 +1,9 @@
+package com.mySRPNT2.entity;
+
+public class _UsuarioBicicleta {
+
+	public _UsuarioBicicleta() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
